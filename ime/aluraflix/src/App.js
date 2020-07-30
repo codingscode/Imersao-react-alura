c071de15d88b18c9685e5d98ca3d5b75910d4b36
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-30 12:25:24 -0300

    parte 3

--- ime/aluraflix/src/App.js
+++ ime/aluraflix/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
         <Menu />
+        
         Oi React
     </div>
   );
--- ime/aluraflix/src/components/Menu/menu.js
+++ ime/aluraflix/src/components/Menu/menu.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import logo from '../../assets/imagens/alura.png'
+import './menu.css'
 
 
 function Menu() {
     return (
         <nav>
-            Aluraflix
+            <img className="Logo" src={logo} alt="aluraflix" />
+            
         </nav>
     )
 }
