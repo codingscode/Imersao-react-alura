831ae5e2a9d467e65e8d2768bafc9c5f119ffaf6
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-31 19:40:20 -0300

    parte 15

--- ime/aluraflix/src/index.js
+++ ime/aluraflix/src/index.js
@@ -6,12 +6,20 @@ import App from './App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
+function CadastroVideo() {
+  return (
+     <div>
+        Página de Cadastro de Vídeo
+     </div>
+  )
+}
+
+
 ReactDOM.render(
-  /* <React.StrictMode>
-    <App />
-  </React.StrictMode>, */
+  
   <BrowserRouter>
       <Switch>
+          <Route path="/cadastro/video" component={CadastroVideo} />
           <Route path="/" component={App} />
           
       </Switch>
