d8c2f2692d4754afa9bb2ecb6ebae2cb49842859
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-31 12:07:50 -0300

    parte 12

--- ime/aluraflix/src/components/BannerPrincipal/components/VideoIframeResponsive/comp_banner.js
+++ ime/aluraflix/src/components/BannerPrincipal/components/VideoIframeResponsive/comp_banner.js
@@ -6,7 +6,7 @@ function YouTubeIframeResponsive({ youtubeID }) {
   return (
     <VideoContainer>
       <ResponsiveIframe
-        title="Titulo do Iframe"
+        titulo="Titulo do Iframe"
         src={`https://www.youtube.com/embed/${youtubeID}?autoplay=0&mute=1`}
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
