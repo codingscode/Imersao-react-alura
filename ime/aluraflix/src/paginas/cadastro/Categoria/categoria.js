b43550099a4857ea8b748f59efeb41c5877c7048
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-02 20:30:57 -0300

    parte 32

--- ime/aluraflix/src/paginas/cadastro/Categoria/categoria.js
+++ ime/aluraflix/src/paginas/cadastro/Categoria/categoria.js
@@ -7,9 +7,7 @@ function CadastroCategoria() {
 
     const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial')
 
-    console.log('nomeDaCategoria', nomeDaCategoria)
-
-
+    
     return (
        <PaginaDefault>
           <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
@@ -17,7 +15,7 @@ function CadastroCategoria() {
           <form>
                 <label>
                     Nome da Categoria:
-                    <input type="text" value={nomeDaCategoria} />
+                    <input type="text" value={nomeDaCategoria} onChange={() => { console.log('nomeDaCategoria', nomeDaCategoria)}} />
                 </label>
                 <button>Cadastrar</button>
          </form>
