d7914aecca6030ea72f45739e92ddf3ceadfde0a
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-06 21:26:55 -0300

    parte 81

--- ime/aluraflix/src/repositorios/categ.js
+++ ime/aluraflix/src/repositorios/categ.js
@@ -1,14 +1,19 @@
 import config from '../config/config'
 
-const url_categorias = `${config.url_backend_top}/categorias?_embed=videos`
+const url_categorias = `${config.url_backend_top}/categorias`
 
 function acessarTodoscomVideo() {
 
-    console.log(url_categorias)
+    console.log('*', url_categorias)
 
-    return fetch(url_categorias).then(async (res) => {
-         const resposta = await res.json()
-         return resposta
+    return fetch(`${url_categorias}?_embed=videos`).then(async (res) => {
+         if (res.ok) {
+             const resposta = await res.json()
+             return resposta
+             
+         }
+
+         throw Error('Não foi possível acessar os dados')
     })
 
 }
