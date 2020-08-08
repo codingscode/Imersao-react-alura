8d9b554f3a6930ecdf7c5f13792165c3633493ed
Heronnes Pereira <heronnestg@hotmail.com>
2020-08-07 21:38:46 -0300

    parte 95

--- ime/aluraflix/src/paginas/cadastro/Video/video_js.js
+++ ime/aluraflix/src/paginas/cadastro/Video/video_js.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import PaginaDefault from '../../../components/Paginadefault/paginadefault'
 import useForm from '../../../Hooks/useForm'
 import CampoForm from '../../../components/CampoForm/campoform'
 import Botao from '../../../components/Btn_stl/btn_stl'
 
 function CadastroVideo() {
+    const historico = useHistory()
     const {tratarMudanca, valores} = useForm({})
 
     console.log('video_cadastro***', valores)
@@ -15,11 +16,14 @@ function CadastroVideo() {
 
           <form onSubmit={(evento) => {
                 evento.preventDefault()
+                //eslint-disable-next-line no-alert
                 alert('Video cadastrado com sucesso')
+                historico.push('/')
              }}>
                   <CampoForm etiqueta="Título do Vídeo" nome={"titulo"} valor={valores.titulo} mudanca={tratarMudanca} /> 
+                  <Botao type={"submit"}>Cadastrar</Botao>  {/* tinha botado o botão fora errado */}
+          
           </form>
-          <Botao type="submit">Cadastrar</Botao><br></br>
           <br></br>
           <Link to="/cadastro/categoria" >
               Cadastrar Categoria
